ceca0c4cc825b0eec0e1142823eb9384adbca696
Kyle Mayer <kmayer5120@hotmail.com>
2018-04-03 17:17:32 -0600

    Made a quick person class based on the UML
    We can probably add more to this if needed.

---
+++ Person.java
@@ -0,0 +1,64 @@
+ /*
+  * Author: Kyle Mayer
+  * Email:  kmayer1@cnm.edu
+  * File:   Person.java
+  * Date:   4/3/2018
+  * Course: CSCI2251 Sec 101
+ */
+
+
+class Person
+{
+    //delcare private variables
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person()
+    {
+        //default constructor
+        //set each variable to a default value
+        this.firstName = "";
+        this.lastName = "";
+        this.age = 0;
+    }
+
+    public Person(String firstName, String lastName, int age)
+    {
+        //overloaded constructor
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setAge(age);
+    }
+    
+    //--------Setters and getters for private variables
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstname;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public void setAge(age)
+    {
+        this.age = age;
+    }
+
+    public String getFirstName()
+    {
+        return this.firstName;
+    }
+
+    public String getLastName()
+    {
+        return this.lastName;
+    }
+
+    public String getAge()
+    {
+        return this.age;
+    }
+}
