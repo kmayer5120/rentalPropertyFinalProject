f6b2df55fca52c75604a93c7ac84cbd2e3fe5dc3
Austin Smith <austindsmith15@gmail.com>
2018-04-13 14:18:10 -0600

    Added late date variables for use with statements

--- RentalProperty.java
+++ RentalProperty.java
@@ -3,6 +3,9 @@ public abstract class RentalProperty implements Payable {
   private String rentalID;
   private String propertyDescription;
   private int rentalType;
+  private int moveInDate;
+  private int lateDate;
+  private int secondLateDate;
   private boolean isAvailable;
 
   //Initial constructor
@@ -12,6 +15,9 @@ public abstract class RentalProperty implements Payable {
       this.propertyDescription = "";
       this.rentalType = 0;
       this.isAvailable = false;
+      this.moveInDate = 0;
+      this.lateDate = 0;
+      this.secondLateDate = 0;
   }
 
   public RentalProperty(String rentalID, String propertyDescription,
@@ -22,6 +28,7 @@ public abstract class RentalProperty implements Payable {
       this.setPropertyDescription(propertyDescription);
       this.setRentalType(rentalType);
       this.setIsAvailable(isAvailable);
+      this.setDates(moveInDate);
   }
 
   //Variable set methods
@@ -39,6 +46,12 @@ public abstract class RentalProperty implements Payable {
 
   public void setIsAvailable (boolean isAvailable) {
     this.isAvailable = isAvailable;
+  }
+
+  public void setDates(int moveInDate) {
+    this.moveInDate = moveInDate;
+    this.lateDate = moveInDate + 7;
+    this.secondLateDate = moveInDate + 14;
   }
 
   //Variable get methods
