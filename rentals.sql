0f7eb2e4b1010cf62c20d04cc2ced478713038d9
kmayer5120 <kmayer5120@hotmail.com>
2018-04-16 14:51:36 -0600

    adding rentals.sql to master

---
+++ rentals.sql
@@ -0,0 +1,48 @@
+CREATE TABLE Tenants
+(
+    tenantID INT NOT NULL,
+    firstName varchar(50) NOT NULL,
+    lastName varchar(50) NOT NULL,
+    emailAddress varchar(255) NOT NULL,
+    billingAddress varchar(255) NOT NULL,
+    PRIMARY KEY(tenantID)
+);
+
+CREATE TABLE Properties
+(
+    propertyID varchar(50) NOT NULL,
+    propertyAddress varchar(255) NOT NULL,
+    propertyDescription varchar(255) NOT NULL,
+    isAvailable char(1) NOT NULL,
+    /* 
+     * isLate, isEvicted, and isPaid are basically type boolean.
+     * Their field can take one char. T or F.
+     */
+    isLate char(1), NOT NULL,
+    isEvicted char(1), NOT NULL,
+    isPaid char(1), NOT NULL,
+    leaseTerm INT NOT NULL,
+    rentalFee float(2)
+    moveInDate date,
+    leaseTerm INT NOT NULL,
+    PRIMARY KEY (propertyID)
+);
+
+CREATE TABLE RentedProperties
+(
+    propertyID INT NOT NULL,
+    tenantID INT NOT NULL,
+    FOREIGN KEY (propertyID) REFERENCES Properties (propertyID),
+    FOREIGN KEY (tenantID) REFERENCES Tenants (tenantID)
+);
+
+CREATE TABLE People
+(
+    personID INT NOT NULL,
+    tenantID INT NOT NULL,
+    firstName varchar(50) NOT NULL, 
+    lastName varchar(50) NOT NULL,
+    age INT NOT NULL,
+    PRIMARY KEY (personID),
+    FOREIGN KEY (tenantID) REFERENCES Tenants (tenantID)
+);
