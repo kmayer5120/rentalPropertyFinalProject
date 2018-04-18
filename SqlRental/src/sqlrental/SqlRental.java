5001f61fdb7178f3b389e1cb55ad9394757cd81a
kmayer5120 <kmayer5120@hotmail.com>
2018-04-18 14:01:43 -0600

    fixed indentation and cleaned up auto-generated comments

--- SqlRental/src/sqlrental/SqlRental.java
+++ SqlRental/src/sqlrental/SqlRental.java
@@ -1,42 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sqlrental;
 
-
-
-/**
- *
- * @author danielg
- */
 public class SqlRental
 {
-    /**
-     * @param args the command line arguments
-     */
-          public static void main(String[] args) 
-          {
-                    SqlRental app =new SqlRental();
-                    // TODO code application logic here
-          }
-    
-          public SqlRental()
-          {
-                    //Create db test
-                    DBManager db = new DBManager();
-                    System.out.println(db.createDB("rentals.db"));
+ 
+    public static void main(String[] args) 
+    {
+        SqlRental app =new SqlRental();
+        // TODO code application logic here
+    }
 
-                     //Create Table test
-                    //DBManager db = new DBManager("/home/danielg/test.db");
-                    //Create hashmap
-                    //Key (Column Name), Value (Column Type)
-                    //System.out.println(db.createTable("tblbName"));
+    public SqlRental()
+    {
+        //Create db test
+        DBManager db = new DBManager();
+        System.out.println(db.createDB("rentals.db"));
+        db.createTable();
+         //Create Table test
+        //DBManager db = new DBManager("/home/danielg/test.db");
+        //Create hashmap
+        //Key (Column Name), Value (Column Type)
+        //System.out.println(db.createTable("tblbName"));
 
-                    //Insert Test
-                    //Create hashmap
-                    //Key (Column Name), Value = Value
-                    //System.out.println(db.createTable("tblbName"));
-          }
+        //Insert Test
+        //Create hashmap
+        //Key (Column Name), Value = Value
+        //System.out.println(db.createTable("tblbName"));
+    }
 }
