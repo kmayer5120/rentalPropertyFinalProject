0a3dbe326648a6f40d765bd9656321f2b0b1792c
Kyle Mayer <kmayer5120@hotmail.com>
2018-04-25 19:58:45 -0600

    updated install.sh

--- InstallationInstructions/install.sh
+++ InstallationInstructions/install.sh
@@ -2,12 +2,8 @@
 
 echo "Installing Rental Property Management System"
 
-echo "Creating folder for application"
-mkdir RentalPropertyManagementSystem
-cd RentalPropertyManagementSystem
-
 echo "Retrieving source code from git repository"
 git clone https://github.com/kmayer5120/rentalPropertyFinalProject.git
 
 echo "Installation complete!"
-echo "See read me file for more information."
+echo "See README file for more information."
